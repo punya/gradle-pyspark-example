091cbc2fd0e5ace3b4678ea6c0eb41b94368efd6
Punya Biswal <pbiswal@palantir.com>
2015-04-26 10:12:23 -0400

    Semver

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'artichoke/_version.py'
+versioneer.versionfile_build = None
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = ''
 
 setup(
     name='Artichoke',
-    version='0.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=['artichoke']
 )
